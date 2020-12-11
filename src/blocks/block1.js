92d3d5350e66b7f616d5668c44d21f551905605c
bagleric <bagleric@outlook.com>
2020-12-10 19:32:13 -0700

    add blocks

--- src/blocks/block1.js
+++ src/blocks/block1.js
@@ -13,5 +13,29 @@ Blockly.Blocks["draw_shape_circle"] = {
 Blockly.JavaScript["draw_shape_circle"] = function () {
     var images = require.context("../assets/", false, /\.png$/);
     var url = images("./circle.png");
-    return "<img src="+url+">";
+    return "<img src=" + url + ">";
+};
+
+
+Blockly.Blocks["repeat_block"] = {
+    init: function () {
+        this.jsonInit(theBlocks.repeat_block);
+    }
+};
+
+Blockly.JavaScript["repeat_block"] = function (block) {
+    console.log(block);
+    console.log(block.inputList);
+    return `<div>${block}</div>`;
+};
+
+
+Blockly.Blocks["start_block"] = {
+    init: function () {
+        this.jsonInit(theBlocks.start_block);
+    }
+};
+
+Blockly.JavaScript["start_block"] = function () {
+    return "<div>";
 };
